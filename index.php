dcb4022897bb65399f735ed812beabcbbf7478f6
Reshma <reshmacs8@gmail.com>
2021-06-08 17:27:53 +0530

    class2

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <!-- START PAGE CONTENT -->
 <h1>-----------Master Branch Camp---------</h1>
 <h1>Testtt</h1>
+<h1>CISLIN</h1>
 <p>
 You’ll find EWU in Chicago’s South Loop neighborhood, easily accessible by bus, train and subway. Your professors are experienced professionals, experts in their fields with the ability and willingness to provide the guidance you need to succeed. You’ll also find a financial aid department dedicated to helping you – through part-time employment, grants, scholarships, loans and more – pursue every available means to attend.
 <br/><br/>
@@ -21,4 +22,5 @@ EWU is one of the most cost-effective education institutions in the regions – 
 
 
 <!-- END PAGE CONTENT -->
-<?php 	include_once('assets/includes/footer.php');   ?>
+<?php 	include_once('assets/in
+icludes/footer.php');   ?>
